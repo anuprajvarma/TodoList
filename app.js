16d9bdb983bf68f5411f916304f6bdfa9112b9c9
anuprajvarma <anupraj1854@gmail.com>
2022-06-13 01:00:15 +0530

    connect to database

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
  const express = require("express");
  const bodyParser = require("body-parser");
+ const mongoose = require("mongoose");
 
 const app = express();
-
-var newItems =[];
-var workItem =[];
-
 app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-    
-    var today = new Date();
-    var options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long",
-    }
+var result = [];
 
-    const day = today.toLocaleDateString("en-US",options);
-    res.render("list",{kindDay:day,newItemList:newItems});
+mongoose.connect("mongodb://localhost:27017/todolist").then(()=>console.log("connection is succesful")).catch((err)=>console.log(err));
+
+const todolistSchema = new mongoose.Schema({
+    name: String,
+})
+
+const newItem = new mongoose.model("task",todolistSchema);
+
+app.get("/",function(req,res){
+
+    newItem.find({},function(err,allitems){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("list",{kindDay:"Today",newItemList:allitems});
+        }
+    })
 });
 
 app.post("/",function(req,res){
     
-    var newItem = req.body.task;
+    var task = req.body.task;
     var items= req.body.button;
-
-    if(items==="work"){
-        workItem.push(newItem);
-        res.redirect("/work");
+    if(task===""){
+        res.redirect("/")
     }else{
-        newItems.push(newItem);
-        res.redirect("/");
+        const taskName = new newItem({
+            name:task,
+         })
+         newItem.insertMany([taskName]);
+         res.redirect("/")
     }
 })
 
